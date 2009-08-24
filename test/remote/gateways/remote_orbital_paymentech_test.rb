c75879d747880fe927f21eef800406a88375c6a6
Scott Woods <scott@westarete.com>
2009-08-24 10:43:17 -0400

    Amend criteria for determining success or failure.

--- test/remote/gateways/remote_orbital_paymentech_test.rb
+++ test/remote/gateways/remote_orbital_paymentech_test.rb
@@ -430,12 +430,10 @@ class RemoteOrbitalPaymentechTest < Test::Unit::TestCase
 
               @options[:address][:zip] = avsdata[:zip]
               creditcard = credit_card(number, :verification_value => cvvdata[:cvv])
-              valid = amtdata[:valid] && avsdata[:valid] && cvvdata[:valid]
               debug_values = "cc=#{number}, cvv=#{cvvdata[:cvv]}, amount=#{amtdata[:amount]}, zip=#{avsdata[:zip]}, options=#{@options.inspect}"
 
               tests = lambda do |response|
-                # Tampa seems to return success for everything.
-                # valid ? assert_success(response, fail_message) : assert_failure(response, fail_message)
+                amtdata[:amount] == 100 ? assert_success(response, debug_values) : assert_failure(response, debug_values)
                 assert_equal amtdata[:code], response.params['response_code'], "Bad AMT response: #{response.inspect}\nFailed on values: #{debug_values}"
                 assert_equal cvvdata[:code], response.cvv_result['code'], "Bad CVV response: #{response.inspect}\nFailed on values: #{debug_values}"
                 assert_equal avsdata[:code], response.avs_result['code'], "Bad AVS response: #{response.inspect}\nFailed on values: #{debug_values}"
